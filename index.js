b3b8775d6fd1fb1a31d3abdab497feeae950ef6a
unknown <sc43412@gmail.com>
2022-03-09 00:34:22 +0530

    ok

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.use('/', require('./routes/index'));
 
 
 // use listen method to listen the port
-app.listen(process.env.port || 8080,function(err) {
+app.listen(process.env.PORT || 8080,function(err) {
     if(err){ console.log("fire up server is getting error",err); return}
      console.log("server is running successfully on port :",port);
 })
