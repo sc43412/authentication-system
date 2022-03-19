1f0a86cd162569d859bced7482a5a75524f00d68
unknown <sc43412@gmail.com>
2022-03-19 14:52:43 +0530

    ok

--- mailers/forgotpassword.js
+++ mailers/forgotpassword.js
@@ -6,7 +6,7 @@ module.exports.forgotPassword=function(email,Token){
         from: 'snipersourav060@gmail.com',          // sender address
         to:email ,  // list of receivers
         subject: "Forgot Password",     //  Subject line
-        html: `http://localhost:8000/users/newpassword/?token=${Token}` || `https://sch-deploy-auth-system.herokuapp.com//users/newpassword/?token=${Token}`
+        html:  `https://sch-deploy-auth-system.herokuapp.com//users/newpassword/?token=${Token}`
       },function(err,info){
       if(err){console.log("err in sending mail: ",err); return;}
       console.log("message sent",info);
